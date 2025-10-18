64d73dad6580d2fa2e3341f2fbe7888e33fbac4f
Romel <romelaugustine3@gmail.com>
2025-10-18 12:40:59 +0530

    Added * in server.js for update

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use(passport.session());
 app.use("/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
 
-app.get("/", (req, res) => res.send("✅ Backend is running fine"));
+app.get("/", (req, res) => res.send("✅ Backend is running fine****"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
